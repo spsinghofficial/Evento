9f0631b26d241be326fabef9780a4d86b20b526b
Surinder Pal Singh Sidhu <surindersidhu16@gmail.com>
2021-07-03 18:31:21 -0400

    added domain class model for Activity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -8,6 +8,8 @@ namespace Domain
         public DateTime  Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public string MyProperty { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+        
     }
 }
