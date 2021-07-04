5da7b8ce09b215fcd01a4d4cd9df36d50c4ba615
Surinder Pal Singh Sidhu <surindersidhu16@gmail.com>
2021-07-04 18:08:31 -0400

    using axios to get response, added CORS Policy to allow CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,12 @@ namespace API
             services.AddDbContext<DataContext> (opt => {
             opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
            
         }
 
@@ -53,6 +59,7 @@ namespace API
            // app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
