17ce8191d80a3d97b6a6ff86b2b4ae85a85b7baf
Surinder Pal Singh Sidhu <surindersidhu16@gmail.com>
2021-07-04 18:47:14 -0400

    added Semantic UI React library

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header,List } from 'semantic-ui-react';
 
 function App() {
 
@@ -16,20 +17,16 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          {activities.map((activity:any) => {
+    <div>
+      <Header as ='h2' icon='users' content='Events'/>
+        <List>
+          {activities.map((activity:any) => (
 
-            <li key = {activity.id}> 
-            {activity.id}
-            </li>
-          })}
-
-          
-        </div>
-      </header>
+            <List.Item key = {activity.id}> 
+            {activity.title}
+            </List.Item>
+          ))}
+        </List>
     </div>
  )}
 
